0b2a95542d0659e82f6260e3009f2d4c387bef98
Jeffery Allain <jefferyallain@gmail.com>
2021-02-24 01:56:47 -0600

    Updating tournament_results view to have tournament column

--- views/tournament_results.sql
+++ views/tournament_results.sql
@@ -1,6 +1,7 @@
 CREATE VIEW tournament_results AS
 SELECT
-	r.id,
+	t.name as tournament,
+	r.id as rikishi_id,
 	rm.tournament_id,
 	sum(rm.won) as wins,
 	sum(CASE WHEN rm.won = 0 THEN 1 ELSE 0 END) as losses
@@ -8,4 +9,6 @@ FROM rikishis as r
 JOIN rikishi_matches as rm
 	ON r.id = rm.rikishi_id
 	AND rm.concluded = 1
+JOIN tournaments as t
+	ON t.id = rm.tournament_id
 GROUP BY r.id, rm.tournament_id
